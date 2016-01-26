084a47847b16454357d03b186151aa8180e0f12c
Stuart Hiller <hiller.stuart@gmail.com>
2016-01-26 11:20:07 -0800

    merging

--- server/controllers/exampleController.js
+++ server/controllers/exampleController.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/index');
 
 router.get('/', function(req, res){
-  res.send({HI: "HI"});
+  res.send({HI: db.People});
 });
 
 module.exports = router;
