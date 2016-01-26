9655f67053b3b0718e126ad5b32a706f7aed85d9
Wyatt Williams <williams.wyatt@gmail.com>
2016-01-26 12:09:09 -0800

    pic scrape

--- server/controllers/Image.js
+++ server/controllers/Image.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res){
   res.send({HI: db.Img_link.img_link });
 });
 
+pictureAPI = function() {
+	var req = {
+		url: 'https://pixabay.com/api/?key=1957981-8991433880a207a37bf64bff4',
+		method: 'GET',
+	}
+}
+
 module.exports = router;
