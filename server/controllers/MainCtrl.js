c12c34d10a03282a19f80a6e0af69ba21f57f01b
rk23 <reed.kinning@gmail.com>
2016-01-26 14:38:57 -0800

    async work

--- server/controllers/MainCtrl.js
+++ server/controllers/MainCtrl.js
@@ -3,18 +3,66 @@
  */
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 var db = require('../models/index');
 
 router.get('/', function(req, res){
 
-  var rand = Math.random() * 100;
+  var PQI = {};
 
-  db.Quote.find().skip
+  var getPeople = function(PQI) {
+    db.People.find({}, function(err, people){
+      if (err) console.log(err);
+      PQI.people = people;
+      return true;
+    })
+  };
 
-  db.Img_link.find(function(err, links) {
-    if (err) return res.status(500).send(err);
-    res.send(links);
+  var getQuotes = function(PQI) {
+    db.Quote.find({}, function(err, quotes){
+      if (err) console.log(err);
+      PQI.quotes = quotes;
+      return true;
+    })
+  };
+
+  var getImages = function(PQI) {
+    db.Img_link.find({}, function(err, imgLinks){
+      if (err) console.log(err);
+      PQI.imageLinks = imgLinks;
+      return true;
+    })
+  };
+
+  async.series([
+      function(PQI) {
+        db.People.find({}, function(err, people){
+          if (err) console.log(err);
+          PQI.people = people;
+          return true;
+        })
+      },
+      function(PQI) {
+        db.Quote.find({}, function(err, quotes){
+          if (err) console.log(err);
+          PQI.quotes = quotes;
+          return true;
+        })
+      },
+      function(PQI) {
+        db.Img_link.find({}, function(err, imgLinks){
+          if (err) console.log(err);
+          PQI.imageLinks = imgLinks;
+          return true;
+        })
+      }
+  ],
+    function(err, results){
+      if (err) console.log("error: " + err);
+      res.send(PQI);
   });
+
+
 });
 
 module.exports = router;
