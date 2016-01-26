90905bd1a47f1aad691c3dbc759da1d8bcbe9733
blakewallan <blakewallan@gmail.com>
2016-01-26 11:57:07 -0800

    DB fixes

--- server/controllers/Image.js
+++ server/controllers/Image.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../models/index');
 
 router.get('/', function(req, res){
-  res.send({HI: db.Img_link.img_link });
+  res.send({HI: db.Img_link.all });
 });
 
 module.exports = router;
