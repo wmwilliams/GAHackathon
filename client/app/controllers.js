b7ff865bb60a8835721dec02614d088a2e8c26ea
blakewallan <blakewallan@gmail.com>
2016-01-26 16:20:16 -0800

    added services and controller changes

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,6 +1,6 @@
 angular.module('HackathonCtrls', ['DataServices'])
 .controller('HomeCtrl', ['$scope', 'AllData', function($scope, AllData) {
-	$scope.people = AllData;
+	$scope.people = AllData.get();
 	console.log($scope)
 }])
 
--- client/app/controllers/controllers.js
+++
@@ -1,3 +0,0 @@
-// var hackathonCtrl = angular.module('HackathonCtrl');
-
-// hackathonCtrl.cont
