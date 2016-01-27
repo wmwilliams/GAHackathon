50c56115c2a8b6469c6ee720ae64f27e86d39e9f
blakewallan <blakewallan@gmail.com>
2016-01-26 16:47:35 -0800

    stuff

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,7 +1,12 @@
 angular.module('HackathonCtrls', ['DataServices'])
 .controller('HomeCtrl', ['$scope', 'AllData', function($scope, AllData) {
 
-	$scope.data = AllData.get();
-	console.log($scope.data)
+	$scope.data = AllData.get(function success(data){
+    $scope.people = data.people;
+    $scope.images = data.imageLinks;
+  }, function error(data){
+    //console.log(data);
+  });
+  console.log($scope.images);
 }]);
 
