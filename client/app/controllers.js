3628b11bc3e22f949e50cd1bd14692f8cb1536b7
rk23 <reed.kinning@gmail.com>
2016-01-26 16:45:26 -0800

    working

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,6 +1,20 @@
 angular.module('HackathonCtrls', ['DataServices'])
 .controller('HomeCtrl', ['$scope', 'AllData', function($scope, AllData) {
-	$scope.people = AllData;
-	console.log($scope)
+	AllData.get(
+    function success(data, stuff){
+
+      var randPerson = Math.floor((Math.random() * data.people.length) + 1);
+      $scope.person = data.people[randPerson];
+
+      var randQuote = Math.floor((Math.random() * data.quotes.length) + 1);
+      $scope.quote = data.quotes[randQuote];
+
+      var randImage = Math.floor((Math.random() * data.imageLinks.length) + 1);
+      $scope.image = data.imageLinks[randImage];
+
+    },
+    function error(data){
+      console.log(data)
+    });
 }])
 
