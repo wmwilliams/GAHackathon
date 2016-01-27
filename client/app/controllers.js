19eaa78a3448a8aaa3fdbecd4a6e590f29be0ded
Wyatt Williams <williams.wyatt@gmail.com>
2016-01-26 16:19:49 -0800

    working with data obj

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,6 +1,6 @@
 angular.module('HackathonCtrls', ['DataServices'])
 .controller('HomeCtrl', ['$scope', 'AllData', function($scope, AllData) {
-	$scope.people = AllData;
-	console.log($scope)
+	$scope.data = AllData.get();
+	console.log($scope.data)
 }])
 
--- client/app/services.js
+++ client/app/services.js
@@ -1,5 +1,5 @@
 var appServices = angular.module('DataServices', ['ngResource']);
 
 appServices.factory('AllData', ['$resource', function($resource) {
-	return $resource('http://localhost:3000//api/getAll')
+	return $resource('http://localhost:3000/api/getAll')
 }])
