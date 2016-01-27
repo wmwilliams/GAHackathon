bccf40d8176a5110ed81883bf1371065e6feab47
Wyatt Williams <williams.wyatt@gmail.com>
2016-01-26 16:45:19 -0800

    deleted extra views

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,7 +1,6 @@
 angular.module('HackathonCtrls', ['DataServices'])
 .controller('HomeCtrl', ['$scope', 'AllData', function($scope, AllData) {
-
 	$scope.data = AllData.get();
 	console.log($scope.data)
-}]);
+}])
 
