4124cd87b79c0031b2ee97fac9ecb817af396331
Wyatt Williams <williams.wyatt@gmail.com>
2016-03-01 19:21:57 -0800

    test

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -21,20 +21,23 @@ angular.module('HackathonCtrls', ['DataServices'])
     });
   $scope.clickMe = function () {
       AllData.get(
-      function success(data, stuff){
+    function success(data, stuff){
 
-        var randPerson = Math.floor((Math.random() * data.people.length) + 1);
-        $scope.person = data.people[randPerson];
+      var randPerson = Math.floor((Math.random() * data.people.length) + 1);
+      $scope.person = data.people[randPerson];
+      console.log($scope.person);
 
-        var randQuote = Math.floor((Math.random() * data.quotes.length) + 1);
-        $scope.quote = data.quotes[randQuote];
+      var randQuote = Math.floor((Math.random() * data.quotes.length) + 1);
+      $scope.quote = data.quotes[randQuote];
+      console.log($scope.quote);
 
-        var randImage = Math.floor((Math.random() * data.imageLinks.length) + 1);
-        $scope.image = data.imageLinks[randImage];
+      var randImage = Math.floor((Math.random() * data.imageLinks.length) + 1);
+      $scope.image = data.imageLinks[randImage];
+      console.log($scope.image);
 
-      },
+    },
       function error(data){
         console.log(data)
       })
-    }  
+    }
 }]);
