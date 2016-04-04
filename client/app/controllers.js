0f066ac093f80cea4020660df26f3f5b7a8579b4
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-04 16:46:17 -0700

    test

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -13,52 +13,52 @@ angular.module('HackathonCtrls', ['DataServices'])
       $scope.data.people = jsonData.people;
       $scope.data.quotes = jsonData.quotes;
       $scope.data.imageLinks = jsonData.imageLinks;
-
-      if(!$scope.data.people)  {
-        AllData.get(
-          function success(data, stuff){
-            var jsonData = angular.fromJson(data);
-            $scope.data.people = jsonData.people;
-          },
-          function error(data) {
-            console.log(data);
-          }
-        )
-      } else if(!$scope.data.quotes) {
-        AllData.get(
-          function success(data, stuff){
-            var jsonData = angular.fromJson(data);
-            $scope.data.quotes = jsonData.quotes;
-          },
-          function error(data) {
-            console.log(data);
-          }
-        )
-      } else if(!$scope.data.imageLinks) {
-        AllData.get(
-          function success(data, stuff){
-            var jsonData = angular.fromJson(data);
-            $scope.data.imageLinks = jsonData.imageLinks;
-          },
-          function error(data) {
-            console.log(data);
-          }
-        )
-      } else if ($scope.data.imageLinks && $scope.data.people && $scope.data.quotes){
-        var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
-        $scope.person = $scope.data.people[randPerson];
-
-        var randQuote = Math.floor((Math.random() * $scope.data.quotes.length) + 1);
-        $scope.quote = $scope.data.quotes[randQuote];
-
-        var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
-        $scope.image = $scope.data.imageLinks[randImage];
-      }
     },
     function error(data) {
       console.log(data);
     }
   )
+
+  if(!$scope.data.people)  {
+    AllData.get(
+      function success(data, stuff){
+        var jsonData = angular.fromJson(data);
+        $scope.data.people = jsonData.people;
+      },
+      function error(data) {
+        console.log(data);
+      }
+    )
+  } else if(!$scope.data.quotes) {
+    AllData.get(
+      function success(data, stuff){
+        var jsonData = angular.fromJson(data);
+        $scope.data.quotes = jsonData.quotes;
+      },
+      function error(data) {
+        console.log(data);
+      }
+    )
+  } else if(!$scope.data.imageLinks) {
+    AllData.get(
+      function success(data, stuff){
+        var jsonData = angular.fromJson(data);
+        $scope.data.imageLinks = jsonData.imageLinks;
+      },
+      function error(data) {
+        console.log(data);
+      }
+    )
+  } else if ($scope.data.imageLinks && $scope.data.people && $scope.data.quotes){
+    var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
+    $scope.person = $scope.data.people[randPerson];
+
+    var randQuote = Math.floor((Math.random() * $scope.data.quotes.length) + 1);
+    $scope.quote = $scope.data.quotes[randQuote];
+
+    var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
+    $scope.image = $scope.data.imageLinks[randImage];
+  }
 
   $scope.clickMe = function () {
       var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
