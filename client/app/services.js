5fc136918554780647d2fafe27aa2fb6cb6e500f
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-04 13:42:47 -0700

    test

--- client/app/services.js
+++ client/app/services.js
@@ -1,7 +1,7 @@
 var appServices = angular.module('DataServices', ['ngResource']);
 
 appServices.factory('AllData', ['$resource', function($resource) {
-	return $resource('http://forgotten-quotes.herokuapp.com/api/getall');
+	return $resource('https://forgotten-quotes.herokuapp.com/api/getall');
            // return $resource('http://localhost:3000/api/getall');
 }]);
 
