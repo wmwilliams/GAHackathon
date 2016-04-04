0aa0e22e6bcd92bd66fe49093e33883919469416
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-04 14:53:12 -0700

    test

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,44 +1,38 @@
 angular.module('HackathonCtrls', ['DataServices'])
 .controller('HomeCtrl', ['$scope', 'AllData', function($scope, AllData) {
-  $scope.data = {};
+  $scope.data = {
+  };
 
   AllData.get(
     function success(data, stuff){
 
       var jsonData = angular.fromJson(data);
-      console.log(jsonData.people);
+      $scope.data.people = jsonData.people;
+      $scope.data.quotes = jsonData.quotes;
+      $scope.data.imgLinks = jsonData.imageLinks;
 
-      var randPerson = Math.floor((Math.random() * data.people.length) + 1);
-      $scope.person = data.people[randPerson];
+      var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
+      $scope.person = $scope.data.people[randPerson];
 
-      var randQuote = Math.floor((Math.random() * data.quotes.length) + 1);
-      $scope.quote = data.quotes[randQuote];
+      var randQuote = Math.floor((Math.random() * $scope.data.quotes.length) + 1);
+      $scope.quote = $scope.data.quotes[randQuote];
 
-      var randImage = Math.floor((Math.random() * data.imageLinks.length) + 1);
-      $scope.image = data.imageLinks[randImage];
+      var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
+      $scope.image = $scope.data.imageLinks[randImage];
     },
     function error(data){
       console.log(data)
     });
   $scope.clickMe = function () {
-      AllData.get(
-        function success(data, stuff){
 
-        var jsonData = angular.fromJson(data);
-        console.log(jsonData.quotes);
+          var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
+          $scope.person = $scope.data.people[randPerson];
 
-          var randPerson = Math.floor((Math.random() * data.people.length) + 1);
-          $scope.person = data.people[randPerson];
+          var randQuote = Math.floor((Math.random() * $scope.data.quotes.length) + 1);
+          $scope.quote = $scope.data.quotes[randQuote];
 
-          var randQuote = Math.floor((Math.random() * data.quotes.length) + 1);
-          $scope.quote = data.quotes[randQuote];
+          var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
+          $scope.image = $scope.data.imageLinks[randImage];
 
-          var randImage = Math.floor((Math.random() * data.imageLinks.length) + 1);
-          $scope.image = data.imageLinks[randImage];
-
-      },
-      function error(data){
-        console.log(data)
-      })
     }
 }]);
