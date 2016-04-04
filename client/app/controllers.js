ca3d599c972db653e19855062669b408e5107995
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-04 15:54:33 -0700

    test

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -10,21 +10,6 @@ angular.module('HackathonCtrls', ['DataServices'])
       $scope.data.quotes = jsonData.quotes;
       $scope.data.imageLinks = jsonData.imageLinks;
 
-      var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
-      $scope.person = $scope.data.people[randPerson];
-
-      var randQuote = Math.floor((Math.random() * $scope.data.quotes.length) + 1);
-      $scope.quote = $scope.data.quotes[randQuote];
-
-      var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
-      $scope.image = $scope.data.imageLinks[randImage];
-    },
-    function error(data){
-      console.log(data)
-    }
-  );
-
-  $scope.clickMe = function () {
       if($scope.data.people.length <= 10 || $scope.data.quotes <= 10 || $scope.data.imageLinks <= 10) {
         AllData.get(
           function success(data, stuff){
@@ -38,6 +23,23 @@ angular.module('HackathonCtrls', ['DataServices'])
           }
         )
       } else {
+
+        var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
+        $scope.person = $scope.data.people[randPerson];
+
+        var randQuote = Math.floor((Math.random() * $scope.data.quotes.length) + 1);
+        $scope.quote = $scope.data.quotes[randQuote];
+
+        var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
+        $scope.image = $scope.data.imageLinks[randImage];
+      }
+    },
+    function error(data){
+      console.log(data)
+    }
+  );
+
+  $scope.clickMe = function () {
       var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
       $scope.person = $scope.data.people[randPerson];
 
@@ -46,6 +48,5 @@ angular.module('HackathonCtrls', ['DataServices'])
 
       var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
       $scope.image = $scope.data.imageLinks[randImage];
-    };
   };
 }]);
