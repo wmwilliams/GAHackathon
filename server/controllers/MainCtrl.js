dbdf149b6e9ba78e9347a0ab091f62b7c403fc4d
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-04 17:26:58 -0700

    test

--- server/controllers/MainCtrl.js
+++ server/controllers/MainCtrl.js
@@ -9,39 +9,21 @@ router.get('/', function(req, res){
 
   var PQI = {};
 
-  db.People.find({}, function(err, people){
-    PQI.people = people;
-    if (err) console.log(err);
-  });
-  db.Quote.find({}, function(err, quotes){
-    PQI.quotes = quotes;
-    if (err) console.log(err);
-  });
-  db.Img_link.find({}, function(err, imgLinks){
-    PQI.imageLinks = imgLinks;
-    if (err) console.log(err);
-  });
-
-  if(PQI.imageLinks && PQI.people && PQI.quotes) {
-    return res.send(PQI);
-  } else if (!PQI.imageLInks) {
-    db.Img_link.find({}, function(err, imgLinks){
-      PQI.imageLinks = imgLinks;
-      if (err) console.log(err);
-    });
-  }
-  else if(!PQI.quotes) {
-    db.Img_link.find({}, function(err, imgLinks){
-      PQI.quotes = quotes;
-      if (err) console.log(err);
-    });
-  }
-  else if(!PQI.people) {
     db.People.find({}, function(err, people){
       PQI.people = people;
       if (err) console.log(err);
     });
-  }
+    db.Quote.find({}, function(err, quotes){
+      PQI.quotes = quotes;
+      if (err) console.log(err);
+    });
+    db.Img_link.find({}, function(err, imgLinks){
+      PQI.imageLinks = imgLinks;
+      if (err) console.log(err);
+    });
+    if(PQI.imageLinks && PQI.people && PQI.quotes) {
+      return res.send(PQI);
+    }
 });
 
 module.exports = router;
