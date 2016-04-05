2d2bc9299cf23921f0a60c89a0d9aab829671032
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-04 17:03:15 -0700

    test

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -14,46 +14,49 @@ angular.module('HackathonCtrls', ['DataServices'])
       $scope.data.quotes = jsonData.quotes;
       $scope.data.imageLinks = jsonData.imageLinks;
 
-      if(!$scope.data.people)  {
-        AllData.get(
-          function success(data, stuff){
-            var jsonData = angular.fromJson(data);
-            $scope.data.people = jsonData.people;
-          },
-          function error(data) {
-            console.log(data);
-          }
-        )
-      } else if(!$scope.data.quotes) {
-        AllData.get(
-          function success(data, stuff){
-            var jsonData = angular.fromJson(data);
-            $scope.data.quotes = jsonData.quotes;
-          },
-          function error(data) {
-            console.log(data);
-          }
-        )
-      } else if(!$scope.data.imageLinks) {
-        AllData.get(
-          function success(data, stuff){
-            var jsonData = angular.fromJson(data);
-            $scope.data.imageLinks = jsonData.imageLinks;
-          },
-          function error(data) {
-            console.log(data);
-          }
-        )
-      } else if ($scope.data.imageLinks && $scope.data.people && $scope.data.quotes){
-        var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
-        $scope.person = $scope.data.people[randPerson];
+      console.log($scope.data.people);
+      console.log($scope.data.quotes);
+      console.log($scope.data.imageLinks);
+      // if(!$scope.data.people)  {
+      //   AllData.get(
+      //     function success(data, stuff){
+      //       var jsonData = angular.fromJson(data);
+      //       $scope.data.people = jsonData.people;
+      //     },
+      //     function error(data) {
+      //       console.log(data);
+      //     }
+      //   )
+      // } else if(!$scope.data.quotes) {
+      //   AllData.get(
+      //     function success(data, stuff){
+      //       var jsonData = angular.fromJson(data);
+      //       $scope.data.quotes = jsonData.quotes;
+      //     },
+      //     function error(data) {
+      //       console.log(data);
+      //     }
+      //   )
+      // } else if(!$scope.data.imageLinks) {
+      //   AllData.get(
+      //     function success(data, stuff){
+      //       var jsonData = angular.fromJson(data);
+      //       $scope.data.imageLinks = jsonData.imageLinks;
+      //     },
+      //     function error(data) {
+      //       console.log(data);
+      //     }
+      //   )
+      // } else if ($scope.data.imageLinks && $scope.data.people && $scope.data.quotes){
+      //   var randPerson = Math.floor((Math.random() * $scope.data.people.length) + 1);
+      //   $scope.person = $scope.data.people[randPerson];
 
-        var randQuote = Math.floor((Math.random() * $scope.data.quotes.length) + 1);
-        $scope.quote = $scope.data.quotes[randQuote];
+      //   var randQuote = Math.floor((Math.random() * $scope.data.quotes.length) + 1);
+      //   $scope.quote = $scope.data.quotes[randQuote];
 
-        var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
-        $scope.image = $scope.data.imageLinks[randImage];
-      }
+      //   var randImage = Math.floor((Math.random() * $scope.data.imageLinks.length) + 1);
+      //   $scope.image = $scope.data.imageLinks[randImage];
+      // }
     },
     function error(data) {
       console.log(data);
