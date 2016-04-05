9430ec6967c617def384f9ea58f15e06d5c752e0
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-05 11:03:05 -0700

    test

--- server/controllers/MainCtrl.js
+++ server/controllers/MainCtrl.js
@@ -6,31 +6,31 @@ var router = express.Router();
 var db = require('../models/index');
 
 router.get('/', function(req, res){
-
+  getPeople();
   var PQI = {};
 
-var getPeople = function(PQI) {
-  db.People.find({}, function(err, people){
-    if (err) console.log(err);
-    PQI.people = people;
-  });
-  getImages();
-};
-var getImages = function(PQI) {
-  db.Img_link.find({}, function(err, imgLinks){
-    if (err) console.log(err);
-    PQI.imageLinks = imgLinks;
-  });
-  getQuotes();
-}
-var getQuotes = function(PQI) {
-  db.Quote.find({}, function(err, quotes){
-    if (err) console.log(err);
-    PQI.quotes = quotes;
-    return res.send(PQI)
-  });
-}
-getPeople();
+  var getPeople = function(PQI) {
+    db.People.find({}, function(err, people){
+      if (err) console.log(err);
+      PQI.people = people;
+    });
+    getImages();
+  };
+  var getImages = function(PQI) {
+    db.Img_link.find({}, function(err, imgLinks){
+      if (err) console.log(err);
+      PQI.imageLinks = imgLinks;
+    });
+    getQuotes();
+  }
+  var getQuotes = function(PQI) {
+    db.Quote.find({}, function(err, quotes){
+      if (err) console.log(err);
+      PQI.quotes = quotes;
+      return res.send(PQI)
+    });
+  }
+  console.log(getPeople())
 });
 
 module.exports = router;
