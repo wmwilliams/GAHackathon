50ad35089abb57cc3d34cf836831d4f983805a70
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-05 11:00:13 -0700

    test

--- server/controllers/MainCtrl.js
+++ server/controllers/MainCtrl.js
@@ -1,4 +1,4 @@
-/**
+  /**
  * Created by ReedK on 1/26/16.
  */
 var express = require('express');
@@ -9,19 +9,28 @@ router.get('/', function(req, res){
 
   var PQI = {};
 
+var getPeople = function(PQI) {
   db.People.find({}, function(err, people){
     if (err) console.log(err);
     PQI.people = people;
   });
+  getImages();
+};
+var getImages = function(PQI) {
   db.Img_link.find({}, function(err, imgLinks){
     if (err) console.log(err);
     PQI.imageLinks = imgLinks;
   });
+  getQuotes();
+}
+var getQuotes = function(PQI) {
   db.Quote.find({}, function(err, quotes){
     if (err) console.log(err);
     PQI.quotes = quotes;
     return res.send(PQI)
   });
+}
+getPeople();
 });
 
 module.exports = router;
