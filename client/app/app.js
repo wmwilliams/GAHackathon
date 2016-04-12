12f2416bc1dabc6e1f8ddb8e3c9d0526d3037928
Wyatt Williams <williams.wyatt@gmail.com>
2016-04-12 13:46:48 -0700

    test

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,22 @@
 var app = angular.module('HackathonApp', ['ngRoute', 'ngError', 'HackathonCtrls']);
 
+app.directive('ngError', ['$parse', function($parse){
+    return {
+        restrict: 'A',
+        compile: function($element, attr) {
+            var fn = $parse(attr['ngError']);
+
+            return function(scope, element, attr) {
+                element.on('error', function(event) {
+                    scope.$apply(function() {
+                        fn(scope, {$event:event});
+                    });
+                });
+            };
+        }
+    };
+}]);
+
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
     .when('/', {
